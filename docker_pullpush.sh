01efe382f8398d2c4c99e3d2f856813de5e5496a
Joel Castillo <jcastillo@hyperfine.io>
2021-12-03 11:52:07 -0500

    Do not use named profile for AWS

--- docker_pullpush.sh
+++ docker_pullpush.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [[ ${#6} -gt 0 ]]; then
-    profile="--profile $6"
-fi
-
-docker pull $1
-aws ecr get-login-password --region $2 $profile | docker login --username AWS --password-stdin $3.dkr.ecr.$2.amazonaws.com
-docker tag $1 $3.dkr.ecr.$2.amazonaws.com/$4:$5
-docker push $3.dkr.ecr.$2.amazonaws.com/$4:$5
+docker pull "$1"
+aws ecr get-login-password --region "$2" | docker login --username AWS --password-stdin "$3.dkr.ecr.$2.amazonaws.com"
+docker tag "$1" "$3.dkr.ecr.$2.amazonaws.com/$4:$5"
+docker push "$3.dkr.ecr.$2.amazonaws.com/$4:$5"
